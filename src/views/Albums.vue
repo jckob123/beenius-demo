8e8c1feac66624d577a543a154df9a6db9553c87
DESKTOP-HH7BARM\Kobja <jceferin@gmail.com>
2021-08-12 16:07:37 +0200

    Added prototype albums page

---
+++ src/views/Albums.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="about">
+    <h1>{{ $route.params.id }}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Albums",
+  data(){
+    return {
+      albums: []
+    }
+  }
+});
+</script>
+
+<style>
+</style>
