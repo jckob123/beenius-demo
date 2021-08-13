6d5eea3d459356e8b7ee53f347b7d93a0d0e41d0
DESKTOP-HH7BARM\Kobja <jceferin@gmail.com>
2021-08-13 12:37:27 +0200

    router settings for albums

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,18 +2,24 @@ import { createRouter, createWebHashHistory } from "vue-router";
 import Users from "@/views/Users.vue";
 import Albums from "@/views/Albums.vue";
 
-
 const routes = [
   {
     path: "/users",
     name: "Users",
-    component: Users
+    component: Users,
   },
   {
-    path: "/albums/:id",
+    path: "/albums",
     name: "Albums",
-    component: Albums
-  }
+    component: Albums,
+    children: [
+      {
+        path: ":id",
+        name: "Albums",
+        component: Albums,
+      },
+    ],
+  },
 ];
 
 const router = createRouter({
