c99c2d4bda3972e39a01c4894b89212d12182e86
DESKTOP-HH7BARM\Kobja <jceferin@gmail.com>
2021-08-14 19:25:55 +0200

    Added albums page (prototype)

---
+++ src/components/AlbumCard.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="album-card-container">
+    <div class="photos-container">
+      <img
+        v-for="photo in this.thumbnailPhotos()"
+        :key="photo.id"
+        :src="photo.thumbnailUrl"
+      />
+    </div>
+
+    <div>
+      {{ albumTitle }}
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "album-card",
+  props: {
+    albumTitle: String,
+  },
+  data: function () {
+    return {};
+  },
+  methods: {
+    thumbnailPhotos() {
+      return this.$attrs.albumPhotos;
+    },
+  },
+});
+</script>
+
+<style>
+.album-card-container {
+  position: relative;
+  display: flex;
+  flex-flow: column;
+  justify-content: center;
+  width: 15rem;
+  height: 10rem;
+  margin: 6rem 1rem 1rem 1rem;
+  align-items: center;
+  padding: 8px;
+  -webkit-box-shadow: 0px 0px 21px -2px rgba(0, 0, 0, 0.1);
+  box-shadow: 0px 0px 21px -2px rgba(0, 0, 0, 0.1);
+  transition: all 200ms ease;
+  text-decoration: none;
+  color: inherit;
+}
+.album-card-container:hover {
+  width: 17rem;
+  cursor: pointer;
+  font-size: 1.4rem;
+}
+
+.photos-container {
+  position: absolute;
+  top: -30%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+}
+
+.photos-container img {
+  position: relative;
+  border-radius: 50%;
+  width: 30%;
+  border: 5px solid rgb(226, 226, 226);
+}
+</style>
--- src/views/Albums.vue
+++ src/views/Albums.vue
@@ -1,21 +1,79 @@
 <template>
-  <div class="about">
-    <h1>{{ $route.params.id }}</h1>
+  <div class="content-container">
+    <album-card
+      v-for="album in this.albums"
+      :key="album.id"
+      :albumTitle="album.title"
+      :albumPhotos="album.thumbnailPhotos.map((photo) => photo)"
+    ></album-card>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import albumCard from "@/components/AlbumCard.vue";
+
+interface Album {
+  userId: number;
+  id: number;
+  title: string;
+  thumbnailPhotos: Array<Photo>;
+}
+
+interface Photo {
+  albumId: number;
+  id: number;
+  title: string;
+  url: string;
+  thumbnailUrl: string;
+}
 
 export default defineComponent({
   name: "Albums",
-  data(){
+  components: {
+    albumCard,
+  },
+  data() {
     return {
-      albums: []
-    }
-  }
+      albums: [] as Album[],
+    };
+  },
+  methods: {
+    async getAlbums() {
+      let id_param = this.$route.params.id;
+
+      fetch(
+        `https://jsonplaceholder.typicode.com/albums/${
+          id_param === undefined ? "" : "?userId=" + id_param
+        }`
+      )
+        .then((response) => response.json())
+        //api returns array, loop needed
+        .then((response) => {
+          response.forEach((album: Album) => {
+            this.getFirstThreePhotos(album.id).then((response) => {
+              album.thumbnailPhotos = response;
+              this.albums.push(album);
+            });
+          });
+        });
+    },
+    getFirstThreePhotos(albumId: number) {
+      var promise = fetch(
+        `https://jsonplaceholder.typicode.com/photos?albumId=${albumId}`
+      )
+        .then((response) => response.json())
+        .then((response: Photo[]) => {
+          return response.slice(0, 3);
+        });
+
+      return promise;
+    },
+  },
+  created: function () {
+    this.getAlbums();
+  },
 });
 </script>
 
-<style>
-</style>
+<style></style>
