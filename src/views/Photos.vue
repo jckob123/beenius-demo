c4efbafc828a2d3f89cffcf9b8ad1c24287af4d0
DESKTOP-HH7BARM\Kobja <jceferin@gmail.com>
2021-08-14 19:26:50 +0200

    Added template for Photos page

---
+++ src/views/Photos.vue
@@ -0,0 +1,13 @@
+<template>
+  <div><h1>test</h1></div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "photos",
+});
+</script>
+
+<style></style>
