2552fcab1c820e6d6f9b58ac5046f1a2492184bd
DESKTOP-HH7BARM\Kobja <jceferin@gmail.com>
2021-08-14 19:19:53 +0200

    removed axios (not needed)

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import axios from "axios";
-import VueAxios from "vue-axios";
 
 const app = createApp(App).use(router);
-
-app.use(VueAxios, axios);
 
 //font awesome install
 import { library } from "@fortawesome/fontawesome-svg-core";
