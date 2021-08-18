801da2c3c27df39f25c17bf1bcfc9d4cdb62557d
DESKTOP-HH7BARM\Kobja <jceferin@gmail.com>
2021-08-18 16:26:27 +0200

    Added  random photos on album card

--- src/views/Albums.vue
+++ src/views/Albums.vue
@@ -4,6 +4,7 @@
       v-for="album in this.albums"
       :key="album.id"
       :albumTitle="album.title"
+      :albumId="album.id"
       :albumPhotos="album.thumbnailPhotos.map((photo) => photo)"
     ></album-card>
   </div>
@@ -41,33 +42,44 @@ export default defineComponent({
   methods: {
     async getAlbums() {
       let id_param = this.$route.params.id;
-
       fetch(
         `https://jsonplaceholder.typicode.com/albums/${
           id_param === undefined ? "" : "?userId=" + id_param
         }`
       )
         .then((response) => response.json())
-        //api returns array, loop needed
-        .then((response) => {
+        .then((response: Album[]) => {
+          //api returns array, loop needed
           response.forEach((album: Album) => {
-            this.getFirstThreePhotos(album.id).then((response) => {
+            this.getRandomPhotos(album.id).then((response: Photo[]) => {
               album.thumbnailPhotos = response;
               this.albums.push(album);
             });
           });
         });
     },
-    getFirstThreePhotos(albumId: number) {
+    getRandomPhotos(albumId: number): Promise<Photo[]> {
       var promise = fetch(
         `https://jsonplaceholder.typicode.com/photos?albumId=${albumId}`
       )
         .then((response) => response.json())
         .then((response: Photo[]) => {
-          return response.slice(0, 3);
+          const shuffled = this.shuffleArray(response);
+          return shuffled.splice(0, 3);
         });
 
       return promise;
+    },
+    /*
+     Durstenfeld shuffle by stackoverflow answer: 
+     https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array/12646864#12646864
+    */
+    shuffleArray(array: Photo[]): Photo[] {
+      for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+      }
+      return array;
     },
   },
   created: function () {
