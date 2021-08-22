979e882ca90d14a71dc734b393cedc5ddeafd046
DESKTOP-HH7BARM\Kobja <jceferin@gmail.com>
2021-08-22 14:33:19 +0200

    removed unnecesary imports

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,15 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 const app = createApp(App).use(router);
 
+
 //font awesome install
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-
-library.add(faUserSecret);
-
 app.component("font-awesome-icon", FontAwesomeIcon);
 
 //css
 require("./assets/global.css");
-
 //main mount
 app.mount("#app");
